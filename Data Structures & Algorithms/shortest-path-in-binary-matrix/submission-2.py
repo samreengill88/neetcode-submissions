6e3ed9e04600a2dec4b0590eb9f60579f37d073e
samreengill88 <135256155+samreengill88@users.noreply.github.com>
2026-06-26 23:55:53 -0400

    Add: shortest-path-in-binary-matrix - submission-2

---
+++ Data Structures & Algorithms/shortest-path-in-binary-matrix/submission-2.py
@@ -0,0 +1,36 @@
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        
+        ROWS, COLS = len(grid), len(grid[0])
+        
+        # if start and end cells are blocked -> return -1
+        if grid[0][0] == 1 or grid[ROWS - 1][COLS - 1] == 1:
+            return -1
+        
+        visited = set()
+        pathLen = 0
+
+        directions = [[0, 1], [0, - 1], [1, 0], [-1, 0], [1, 1], [1, -1], [-1, 1], [-1, -1]]
+        
+        q = deque()
+        q.append([0, 0, 1])
+        visited.add((0, 0))
+        
+        while q:
+            row, col, length = q.popleft()
+
+            if row == ROWS - 1 and col == COLS - 1:
+                return length
+
+            for dr, dc in directions:
+                nr, nc = row + dr, col + dc
+
+                
+                # check if cell in bounds and cell == 0 and cell not visited
+                if nr in range(ROWS) and nc in range(COLS) and grid[nr][nc] == 0 and (nr, nc) not in visited:
+
+                    # check if you are at the bottom right cell
+                    
+                    visited.add((nr, nc))
+                    q.append([nr, nc, length + 1])
+        return -1 
