725c9dd3f3fefca620c86850059e2f40e8d1ac69
samreengill88 <135256155+samreengill88@users.noreply.github.com>
2026-06-30 19:40:06 -0400

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.py
@@ -0,0 +1,30 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        
+        # use minHeap
+        # minHeap - always has k largest elements 
+
+        self.minHeap = nums
+        self.k = k
+
+        # minHeap
+        heapq.heapify(self.minHeap)
+
+        # to have k largest elements in heap, pop min elements from heap until len(heap) == k
+        while len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap)
+
+        # now minHeap has k largest elements
+
+    def add(self, val: int) -> int:
+
+        heapq.heappush(self.minHeap, val)
+        
+        # now heap has more than k elements so pop smallest element
+        if len(self.minHeap) > self.k:
+            heapq.heappop(self.minHeap) 
+        return self.minHeap[0]
+        # what if heap is empty at the beginning - do not want to pop in that case -> so pop only if len is > k
+        
+        
