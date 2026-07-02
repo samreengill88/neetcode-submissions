c7bf1ef97579cac00f115f38a442da43e2103ab1
samreengill88 <135256155+samreengill88@users.noreply.github.com>
2026-07-01 22:01:38 -0400

    Add: trapping-rain-water - submission-1

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-1.py
@@ -0,0 +1,59 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        # at each index i : area = min(leftMax, rightMax)  - height[i]
+
+        # we can have 3 arrays of size n = len(height):
+        #           leftMax
+        #           rightMax
+        #           min(leftMax, rightMax) - height[i] and sum all
+        # TC: O(n), SC: O(n)
+        # n = len(height)
+        # if n == 0:
+        #     return 0
+        
+        # leftMax = [0] * n
+        # rightMax = [0] * n
+        # res = 0
+
+        # # leftMax for each index store its leftMax
+        # leftMax[0] = height[0]
+        # for i in range(1, n):
+        #     leftMax[i] = max(height[i], leftMax[i - 1])
+        
+        # rightMax[n - 1] = height[n - 1]
+        # for i in range(n - 2, -1, -1):
+        #     rightMax[i] = max( height[i], rightMax[i + 1])
+        
+        # for i in range(n):
+        #     area =  min(leftMax[i], rightMax[i]) - height[i]
+        #     if area < 0:
+        #         res += 0
+        #     res += area
+        # return res
+    # ===================================2 Pointers=============================================
+
+        n = len(height)
+
+        if n == 0:
+            return 0
+    
+        l = 0
+        r = n - 1
+
+        leftMax = height[0]
+        rightMax = height[n - 1]
+        res = 0
+
+        while l < r:
+            if leftMax < rightMax:
+                l += 1
+                leftMax = max(leftMax, height[l])
+                res += leftMax - height[l]
+        
+            else:
+                r -= 1
+                rightMax = max(rightMax, height[r])
+                res += rightMax - height[r]
+
+        return res
